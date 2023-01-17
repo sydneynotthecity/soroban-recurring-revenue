17bdfc795c9cdc4513465312a7f180474c4a97de
sydneynotthecity <sydney@stellar.org>
2023-01-17 15:50:43 -0600

    Updating var names and add withdraw start epoch

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,4 @@
 #![no_std]
-/// DON'T PANIC!! You don't actually have to change anything here. Yes, you
-/// should read this file to understand what's happening, but there's nothing
-/// that needs to be changed in the code for today.
 
 /// We're using the `soroban_auth` crate today to verify and authenticate users
 /// and some invocations in our contract. It's a really powerful SDK to get
@@ -24,9 +21,10 @@ pub enum Error {
     ContractAlreadyInitialized = 1,
     ContractNotInitialized = 2,
     InvalidAuth = 3,
-    ChildAlreadyWithdrawn = 4,
-    InvalidInvoker = 5,
-    InvalidArguments = 6,
+    ReceiverAlreadyWithdrawn = 4,
+    PrematureFirstWithdraw = 5,
+    InvalidInvoker = 6,
+    InvalidArguments = 7,
 }
 
 /// We are using a `StorageKey` enum to store different types of data, but keying
@@ -35,62 +33,55 @@ pub enum Error {
 #[contracttype]
 #[derive(Clone)]
 pub enum StorageKey {
-    Parent,  // AccountId
-    Child,   // AccountId
+    Sender,  // AccountId
+    Receiver,   // AccountId
     TokenId, // BytesN<32>
+    StartEpoch, // u64
     Amount,  // i128
     Step,    // u64
     Latest,  // u64
 }
 
-/// You know what's a pain? Re-declaring or re-calculating the same value over
-/// and over again. We're going to use the number of seconds in a year more than
-/// once in this contract, so let's use a `const` to declare it once and get it
-/// out of the way.
-const SECONDS_IN_YEAR: u64 = 365 * 24 * 60 * 60; // = 31,536,000 seconds (fyi)
+pub struct RecurringRevenueContract;
 
-pub struct AllowanceContract;
-
-/// Seeing a `trait` may feel familiar. We used one in Quest 4, as well. When
-/// utilizing a trait each of the functions that exist within your contract
-/// implementation must be included in the trait, along with the arguments,
-/// their expected types, and the return type of the function.
-pub trait AllowanceTrait {
+pub trait RecurringRevenueTrait {
     // When `init`ializing the contract, we must specify some of the data that
     // will be stored (remember the `StorageKey`?) for the contract to reference.
-    // We are using an `AccountId` for the `child` to highlight that a transfer
+    // We are using an `AccountId` for the `receiver` to highlight that a transfer
     // from one user to another is the intended use-case of this particular
     // contract. It also makes the Soroban CLI usage a bit cleaner and easier.
     fn init(
         e: Env,
-        child: AccountId,     // the child account receiving the allowance
-        token_id: BytesN<32>, // the id of the token being transferred as an allowance
-        amount: i128,         // the total allowance amount given for the year
+        receiver: AccountId,     // the account receiving the recurring payment
+        token_id: BytesN<32>, // the id of the token being transferred as a payment
+        start_epoch: u64,     // the starting time (in UTC seconds) when the first payment begins
+        amount: i128,         // the amount paid for each recurring payment
         step: u64,            // how frequently (in seconds) a withdrawal can be made
     ) -> Result<(), Error>;
 
-    // When `withdraw` is invoked, a transfer is made from the `Parent` asset
-    // balance to the `Child` asset balance. No signature required!
+    // When `withdraw` is invoked, a transfer is made from the `Sender` asset
+    // balance to the `Receiver` asset balance. No signature required!
     fn withdraw(e: Env) -> Result<(), Error>;
 }
 
 /// When a contract uses "Invoker" authentication, `env.invoker()` returns the
-/// `Address` type. Since we're storing an `AccountId` as the `Parent`, we use
+/// `Address` type. Since we're storing an `AccountId` as the `Sender`, we use
 /// a helper function to convert from one to the other.
 fn to_account(address: Address) -> Result<AccountId, Error> {
     match address {
-        Address::Account(id) => Ok(id),
+        Address::Account(id: AccountId) => Ok(id),
         _ => Err(Error::InvalidInvoker),
     }
 }
 
 #[contractimpl]
-impl AllowanceTrait for AllowanceContract {
+impl RecurringRevenueTrait for RecurringRevenueContract {
     // Remember, before you can invoke `withdraw`, you must invoke `init`
     fn init(
         e: Env,
-        child: AccountId,
+        receiver: AccountId,
         token_id: BytesN<32>,
+        start_epoch: u64,
         amount: i128,
         step: u64,
     ) -> Result<(), Error> {
@@ -110,7 +101,7 @@ impl AllowanceTrait for AllowanceContract {
 
         // A withdrawal should never be `0`. I mean, really. At that point, why
         // even go through the trouble of setting this up?
-        if (amount * step as i128) / SECONDS_IN_YEAR as i128 == 0 {
+        if (amount * step as i128) as i128 == 0 {
             return Err(Error::InvalidArguments);
         }
 
@@ -118,22 +109,14 @@ impl AllowanceTrait for AllowanceContract {
         // ledger here. Nothing fancy here, just the same thing a few times.
         e.storage().set(token_key, token_id);
         e.storage()
-            .set(StorageKey::Parent, to_account(e.invoker()).unwrap()); // the invoker of `init` becomes the `Parent`
-        e.storage().set(StorageKey::Child, child);
+            .set(StorageKey::Sender, to_account(e.invoker()).unwrap()); // the invoker of `init` becomes the `Sender`
+        e.storage().set(StorageKey::Receiver, receiver);
+        e.storage().set(StorageKey::StartEpoch, start_epoch);
         e.storage().set(StorageKey::Amount, amount);
         e.storage().set(StorageKey::Step, step);
 
-        // As an act of goodwill, we set the `Latest` withdraw to be in the past
-        // and allow the `Child` to immediately make the first withdrawal. Just
-        // to get them started, ya know.
-        let current_ts = e.ledger().timestamp();
-        e.storage().set(StorageKey::Latest, current_ts - step);
-        // This is the first time we've used `Env.ledger()` in these contracts.
-        // The Soroban environment, by design, doesn't have a tremendous amount
-        // of context about the current state of the Stellar network. One of the
-        // few things it does know is the `timestamp()` of the most recently
-        // closed ledger on the network. Check in the list of "Further
-        // Resources" in the README to learn more about this.
+        // During contract init() the latest withdraw will be set as a time before the payment start time
+        e.storage().set(StorageKey::Latest, start_epoch - step);
 
         Ok(())
     }
@@ -161,52 +144,50 @@ impl AllowanceTrait for AllowanceContract {
 
         // This part is one of the contract's really nifty tricks. You may have
         // noticed we haven't authenticated the invocation of `withdraw` at all.
-        // That's on purpose! By storing the `Child` in our contract data, we
+        // That's on purpose! By storing the `Receiver` in our contract data, we
         // can ensure they are *always* the beneficiary of the withdrawal. No
-        // matter who actually makes the call to the contract, the child is
-        // always taken care of.
-        let child = e.storage().get(StorageKey::Child).unwrap().unwrap();
+        // matter who actually makes the call to the contract, the receiver
+        // always receives the funds payment.
+        let receiver = e.storage().get(StorageKey::Receiver).unwrap().unwrap();
         // Note: Technically speaking, *anybody* could invoke the `withdraw`
-        // function in the contract (yes, even your cousin Josh). In practice,
-        // for today's quest, the function **must** be invoked by either the
-        // `Parent` or the `Child`.
-
-        // We do some really quick math to figure out a couple things:
-        // - `iterations` - the number of withdraws that can be made in a year
-        // - `amount` - the withdrawn for every iteration
+        // function in the contract.
 
         let step: u64 = e.storage().get(StorageKey::Step).unwrap().unwrap();
-        let iterations = SECONDS_IN_YEAR / step;
         let amount: i128 = e.storage().get(StorageKey::Amount).unwrap().unwrap();
-        let withdraw_amount = amount / iterations as i128;
+
+        // Check that the Receiver is allowed to start receiving payments
+        let start_epoch: u64 = e.storage().get(StorageKey::StartEpoch).unwrap().unwrap();
+        if start_epoch > e.ledger().timestamp(){
+            return Err(Error::PrematureFirstWithdraw)
+        }
 
         // Some more quick math to make sure the `Latest` withdraw occurred *at
         // least* `step` seconds ago. We don't want them draining the piggy bank
         // all at once, after all.
         let latest: u64 = e.storage().get(StorageKey::Latest).unwrap().unwrap();
         if latest + step > e.ledger().timestamp() {
-            return Err(Error::ChildAlreadyWithdrawn);
+            return Err(Error::ReceiverAlreadyWithdrawn);
         }
 
         // This is where the magic happens! We use the client we set up for our
         // token contract earlier to invoke the `xfer_from` function. We're
         // using *this contract's* approval to spend the asset balance of the
-        // `Parent` account to transfer funds *directly* from the `Parent` to
-        // the `Child`. That's amazing! Think of the implications and
+        // `Sender` account to transfer funds *directly* from the `Sender` to
+        // the `Receiver`. That's amazing! Think of the implications and
         // possibilities! They're (and I mean this quite literally) endless!
         client.xfer_from(
             &Signature::Invoker,
             &(0 as i128),
-            &Identifier::Account(e.storage().get(StorageKey::Parent).unwrap().unwrap()),
-            &Identifier::Account(child),
-            &withdraw_amount,
+            &Identifier::Account(e.storage().get(StorageKey::Sender).unwrap().unwrap()),
+            &Identifier::Account(receiver),
+            &amount,
         );
 
         // We quickly set a new `Latest` in our contract data to reflect that
         // another withdraw has taken place. The astute among you may notice
         // this isn't based off the ledger's `timestamp()`, but rather the
-        // latest withdraw. This allows the child to "catch up" on any missed
-        // withdrawals. Very kind of you. You're such a good parent!
+        // latest withdraw. This allows the receiver to "catch up" on any missed
+        // withdrawals. 
         let new_latest = latest + step;
         e.storage().set(StorageKey::Latest, new_latest);
 
--- src/test.rs
+++ src/test.rs
@@ -8,17 +8,17 @@ use soroban_sdk::{
 };
 
 /// The first test function, `test_valid_sequence()`, we test the contract
-/// running in the sequence that is expected: parent approves on the token
-/// contract, parent initializes the AllowanceContract, and child makes some
+/// running in the sequence that is expected: sender approves on the token
+/// contract, sender initializes the RecurringRevenueContract, and receiver makes some
 /// withdraws. Along the way, we check allowances and balances.
 #[test]
 fn test_valid_sequence() {
-    // Just like always, we (say it with me) register the AllowanceContract
+    // Just like always, we (say it with me) register the RecurringRevenueContract
     // contract in a default Soroban environment, and build a client that can be
     // used to invoke the contract.
     let env = Env::default();
-    let contract_id = env.register_contract(None, AllowanceContract);
-    let client = AllowanceContractClient::new(&env, &contract_id);
+    let contract_id = env.register_contract(None, RecurringRevenueContract);
+    let client = RecurringRevenueContractClient::new(&env, &contract_id);
 
     // For this contract, we'll need to set some ledger state to test against.
     // If you do the math, you can tell when we wrote this test!
@@ -31,8 +31,8 @@ fn test_valid_sequence() {
     });
 
     // We create two user accounts to test with, `u1` and `u2`
-    let u1 = env.accounts().generate(); // `Parent` account
-    let u2 = env.accounts().generate(); // `Child` account
+    let u1 = env.accounts().generate(); // `Sender` account
+    let u2 = env.accounts().generate(); // `Receiver` account
 
     // We register a token contract that we can use to test our allowance and
     // payments. For testing purposes, the specific `contract_id` we use doesn't
@@ -87,15 +87,15 @@ fn test_valid_sequence() {
         500000000
     );
 
-    // We invoke the `init` function of the AllowanceContract, providing the
-    // starting arguments. These values result in a weekly allowance of
-    // 9,615,384 stroops (== 0.9615384 units). Why, you big spender!
+    // We invoke the `init` function of the RecurringRevenueContract, providing the
+    // starting arguments. These values result in a weekly payment of
+    // 10,000,000 stroops, or 10 XLM
     client.with_source_account(&u1).init(
-        &u2,                 // our `Child` account
-        &id,                 // our token contract id
-        &500000000,          // 500000000 stroops == 50 units allowance for the year
-        &(7 * 24 * 60 * 60), // 1 withdraw per week (7 days * 24 hours * 60 minutes * 60 seconds)
-    );
+        &u2, 
+        &id, 
+        &1669593600, 
+        &10000000, 
+        &(7 * 24 * 60 * 60));
 
     // We set new ledger state to simulate time passing. Here, we have increased
     // the timestamp by one second.
@@ -107,12 +107,12 @@ fn test_valid_sequence() {
         base_reserve: 10,
     });
 
-    // We invoke the inaugural `withdraw` to get the first allowance paid out.
+    // We invoke the inaugural `withdraw` to get the first payment paid out.
     // Then, we make sure the `u2` account's token balance has increased to
-    // 9,615,384. Note again we don't need any signature here to invoke the
+    // 10,000,000. Note again we don't need any signature here to invoke the
     // `withdraw` function.
     client.withdraw();
-    assert_eq!(token.balance(&Identifier::Account(u2.clone())), 9615384);
+    assert_eq!(token.balance(&Identifier::Account(u2.clone())), 10000000);
 
     // We (again) set new ledger state to simulate time passing. This time,
     // we've increased the timestamp by one week and one second.
@@ -125,9 +125,9 @@ fn test_valid_sequence() {
     });
 
     // We invoke `withdraw` again, and check that the `u2` token balance
-    // reflects two allowance transfers.
+    // reflects two payment transfers.
     client.withdraw();
-    assert_eq!(token.balance(&Identifier::Account(u2.clone())), 9615384 * 2);
+    assert_eq!(token.balance(&Identifier::Account(u2.clone())), 10000000 * 2);
 
     // A third time, we set new ledger state to simulate time passing. Here, we
     // skip ahead two weeks and two seconds from the `init` invocation.
@@ -140,9 +140,9 @@ fn test_valid_sequence() {
     });
 
     // We invoke `withdraw` again, and check that the `u2` token balance now
-    // reflects three allowance transfers.
+    // reflects three payment transfers.
     client.withdraw();
-    assert_eq!(token.balance(&Identifier::Account(u2.clone())), 9615384 * 3);
+    assert_eq!(token.balance(&Identifier::Account(u2.clone())), 10000000 * 3);
 }
 
 /// In our next test function, `test_invalid_sequence()`, we are testing the
@@ -165,8 +165,8 @@ fn test_invalid_sequence() {
         base_reserve: 10,
     });
 
-    let contract_id = env.register_contract(None, AllowanceContract);
-    let client = AllowanceContractClient::new(&env, &contract_id);
+    let contract_id = env.register_contract(None, RecurringRevenueContract);
+    let client = RecurringRevenueContractClient::new(&env, &contract_id);
 
     let id = env.register_contract_token(&BytesN::from_array(
         &env,
@@ -210,7 +210,7 @@ fn test_invalid_sequence() {
 
     client
         .with_source_account(&u1)
-        .init(&u2, &id, &500000000, &(7 * 24 * 60 * 60));
+        .init(&u2, &id, &1669593600, &10000000, &(7 * 24 * 60 * 60));
 
     env.ledger().set(LedgerInfo {
         timestamp: 1669726146,
@@ -221,7 +221,7 @@ fn test_invalid_sequence() {
     });
 
     client.withdraw();
-    assert_eq!(token.balance(&Identifier::Account(u2.clone())), 9615384);
+    assert_eq!(token.balance(&Identifier::Account(u2.clone())), 10000000);
 
     env.ledger().set(LedgerInfo {
         timestamp: 1669726146 + (7 * 24 * 60 * 60) + 1,
@@ -232,7 +232,7 @@ fn test_invalid_sequence() {
     });
 
     client.withdraw();
-    assert_eq!(token.balance(&Identifier::Account(u2.clone())), 9615384 * 2);
+    assert_eq!(token.balance(&Identifier::Account(u2.clone())), 10000000 * 2);
 
     // Ok, stop here! This time, for our third `withdraw` invocation, we are
     // only adding 20 seconds to the previous invocation. Since we've set up for
@@ -251,10 +251,10 @@ fn test_invalid_sequence() {
 }
 
 /// In our next test function, `test_invalid_init()`, we test to make sure that
-/// invoking the AllowanceContract `init` function with invalid arguments will
+/// invoking the RecurringRevenueContract `init` function with invalid arguments will
 /// fail as expected. Specifically, we are passing `0` for the `step` value.
 #[test]
-#[should_panic(expected = "Status(ContractError(6))")] // We want this test to panic since we are giving an unusable argument.
+#[should_panic(expected = "Status(ContractError(7))")] // We want this test to panic since we are giving an unusable argument.
 fn test_invalid_init() {
     // Almost everything in this test is identical to the first one. We'll drop
     // a comment to let you know when things are getting interesting again.
@@ -270,8 +270,8 @@ fn test_invalid_init() {
     let u1 = env.accounts().generate();
     let u2 = env.accounts().generate();
 
-    let contract_id = env.register_contract(None, AllowanceContract);
-    let client = AllowanceContractClient::new(&env, &contract_id);
+    let contract_id = env.register_contract(None, RecurringRevenueContract);
+    let client = RecurringRevenueContractClient::new(&env, &contract_id);
 
     let id = env.register_contract_token(&BytesN::from_array(
         &env,
@@ -319,23 +319,23 @@ fn test_invalid_init() {
     // shut. Also, dividing by zero is impossible. So, that's an important
     // consideration, too.
     client.with_source_account(&u1).init(
-        &u2,        // our `Child` account
+        &u2,        // our `receiver` account
         &id,        // our token contract id
-        &500000000, // 500000000 stroops == 50 units allowance for the year
+        &1669593600, // start epoch for the payments
+        &10000000,    // payment amount of 10XLM
         &0,         // 0 withdraw per second (why would you even do this?)
     );
 
     // Again, there's no need for an assertion here, since this invocation
-    // should fail and respond with `Status(ContractError(6))`.
+    // should fail and respond with `Status(ContractError(7))`.
 }
 
-/// In our final test function, `test_invalid_init_withdrawal()`, we test to make sure that
-/// invoking the AllowanceContract `init` function with invalid arguments will
-/// fail as expected. Specifically, we are passing the arguments so that over the
-/// course of the year, the child can withdraw a portion of the total pot amount of
-/// 1 stroop every second, which is of course impossible.
+/// In our final test function, `test_invalid_premature_withdrawal()`, we test to make sure that
+/// the receiver cannot prematurely withdraw funds from the RecurringRevenueContract.
+/// The contract will init() as expected, but the receiver will be unable to withdraw funds
+/// because they are too early!
 #[test]
-#[should_panic(expected = "Status(ContractError(6))")] // We want this test to panic since we are giving an unusable argument.
+#[should_panic(expected = "Status(ContractError(5))")] // We want this test to panic since we are giving an unusable argument.
 fn test_invalid_init_withdrawal() {
     // Almost everything in this test is identical to the first one. We'll drop
     // a comment to let you know when things are getting interesting again.
@@ -351,8 +351,8 @@ fn test_invalid_init_withdrawal() {
     let u1 = env.accounts().generate();
     let u2 = env.accounts().generate();
 
-    let contract_id = env.register_contract(None, AllowanceContract);
-    let client = AllowanceContractClient::new(&env, &contract_id);
+    let contract_id = env.register_contract(None, RecurringRevenueContract);
+    let client = RecurringRevenueContractClient::new(&env, &contract_id);
 
     let id = env.register_contract_token(&BytesN::from_array(
         &env,
@@ -394,16 +394,16 @@ fn test_invalid_init_withdrawal() {
         500000000
     );
 
-    // Ok, stop here! This time, when invoking `init`, we give a `1` for the
-    // `amount` field and a `1` for the `step` field. If you've followed along
-    // with the math so far, that comes out to 3.1709792e-15 **stroops** per
-    // withdraw. That's even more precision than Microsoft Excel can handle!
+    // Notice that the start epoch is much further in the future
     client.with_source_account(&u1).init(
-        &u2, // our `Child` account
+        &u2, // our `receiver` account
         &id, // our token contract id
-        &1,  // 1 stroops == 0.0000001 units allowance for the year
-        &1,  // 1 withdraw per second
+        &1701129600,  // Future date
+        &10000000, 
+        &(7 * 24 * 60 * 60),  // 1 withdraw per second
     );
+
+    client.withdraw();
 
     // Again, there's no need for an assertion here, since this invocation
     // should fail and respond with `Status(ContractError(6))`.
